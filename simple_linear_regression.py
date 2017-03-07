719c207b96136b07eb94030aaf57ad5111d69034
cponeill <caseyoneill78@hotmail.com>
2017-03-07 11:35:37 -0500

    updating to pep8 standards.

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -10,6 +10,7 @@ import pandas as pd
 training_data = pd.read_csv('../training_data.csv')
 test_data = pd.read_csv('../test_data.csv')
 
+
 # f(x) = a + bX
 def simple_linear_regression(input_feature, output):
     input_sum = input_feature.sum()
@@ -19,7 +20,7 @@ def simple_linear_regression(input_feature, output):
     output_mean = output_sum/N
     in_out_prod = input_feature * output
     in_out_prod_sum = in_out_prod.sum()
-    prod_sum = output_sum * input_sum 
+    prod_sum = output_sum * input_sum
     prod_mean = prod_sum/N
     sqr_test = input_feature * input_feature
     sqr_test_sum = sqr_test.sum()
@@ -31,6 +32,7 @@ def simple_linear_regression(input_feature, output):
 
 
 if __main__ == '__name__':
-    (intercept, slope) = simple_linear_regression(training_data['feature1'], training_data['target'])
+    (intercept, slope) = simple_linear_regression(training_data['feature1'],
+                                                  training_data['target'])
     print("Intercept: ", str(intercept))
     print("Slope    : ", str(slope))
